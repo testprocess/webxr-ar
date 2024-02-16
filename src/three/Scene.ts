b865c101dc5b2590e054cf722d2967475a096953
Dipokal HHJ <hhj@devent.kr>
2024-02-16 22:56:45 +0900

    init: add three

---
+++ src/three/Scene.ts
@@ -0,0 +1,37 @@
+import * as THREE from "three";
+
+class Scene {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    const width = window.innerWidth,
+      height = window.innerHeight;
+
+    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+    camera.position.z = 1;
+
+    const scene = new THREE.Scene();
+
+    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    const material = new THREE.MeshNormalMaterial();
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(width, height);
+    renderer.setAnimationLoop(animation);
+    document.body.appendChild(renderer.domElement);
+
+    function animation(time: number) {
+      mesh.rotation.x = time / 2000;
+      mesh.rotation.y = time / 1000;
+
+      renderer.render(scene, camera);
+    }
+  }
+}
+
+export { Scene };
